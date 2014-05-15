778869243ce22983e17fadc8aadbad30e0d97fad
Mathias Bynens <mathias@qiwi.be>
2014-05-15 15:52:49 +0200

    Tag the v0.1.2 release

--- koi8-u.js
+++ koi8-u.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/koi8-u v0.1.1 by @mathias | MIT license */
+/*! http://mths.be/koi8-u v0.1.2 by @mathias | MIT license */
 ;(function(root) {
 
 	// Detect free variables `exports`.
@@ -118,7 +118,7 @@
 		'labels': [
 			'koi8-u'
 		],
-		'version': '0.1.1'
+		'version': '0.1.2'
 	};
 
 	// Some AMD build optimizers, like r.js, check for specific condition patterns
